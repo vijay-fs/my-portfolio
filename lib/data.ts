ee7a545f428c1cf86b9cd684b6c574c342f92308
vijay-fs <vijaykarthik9868@gmail.com>
2023-10-27 15:20:21 +0530

    initial commit

---
+++ lib/data.ts
@@ -0,0 +1,124 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import corpcommentImg from "@/public/corpcomment.png";
+import mnsafetysolutions from '@/public/mnsafetysolutions.png'
+import eeeblr from '@/public/eeeblr.png'
+import skysecure from '@/public/skysecure.png'
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "AGURCHAND MANMULL JAIN COLLEGE",
+    location: "Chennai, TN",
+    description:
+      "Graduated in B.sc. Computer Science.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2015 - 2018",
+  },
+  {
+    title: "Waterproofing Service (Self Employeed)",
+    location: "Chennai, TN",
+    description:
+      "Did waterproofing service for several construction sites.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2018 - 2021",
+  },
+  {
+    title: "MERN Stack Developer (Freelancer)", 
+    location: "Chennai, TN",
+    description:
+      "I worked as a Freelancer for 6 months. I also upskilled to the full stack.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2022 (Feb) - 2022 (Aug)",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "Bengaluru, KA.",
+    description:
+      "I worked as a full-stack developer in Altsys Technologies Private Limited. My stack includes ReactJS, JavaScript, Tailwind, NodeJS and MySQL.",
+    icon: React.createElement(FaReact),
+    date: "2022 (Aug) - 2022 (Nov)",
+  },
+  {
+    title: "React Native Developer",
+    location: "Bengaluru, KA.",
+    description:
+      "I worked as a React Native developer in Netsecure Technologies. My stack includes React Native, JavaScript and Redux-saga.",
+    icon: React.createElement(FaReact),
+    date: "2023 (Jan) - 2022 (Sep)",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "M.N.safetysolutions",
+    description:
+      "My first React project for M.N.safety solutions. Purpose of this web app is to showcase the hardware products to clients",
+    tags: ["React", "Tailwind", "Figma"],
+    imageUrl: mnsafetysolutions,
+  },
+  {
+    title: "Excellent Engineering Enterprises",
+    description:
+      "Purpose of this web app is to upgrade React to Next app for better SEO visibility, UI & UX.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind"],
+    imageUrl: eeeblr,
+  },
+  {
+    title: "skysecure",
+    description:
+      "This web app is to make Security and Compliance Solutions Simplified!",
+    tags: ["React", "MYSQL", "Tailwind", "ExpressJS"],
+    imageUrl: skysecure,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "React Native",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "MongoDB",
+  "MySQL",
+  "Redux",
+  "GraphQL",
+  "Express",
+  "Python",
+  "Framer Motion",
+] as const;
